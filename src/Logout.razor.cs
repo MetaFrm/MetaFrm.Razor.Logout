9c957b84004af893b6f8639777abb4ab65311b5f
DSUN <dsun@hanmail.net>
2023-06-08 23:01:37 +0900

    DeleteToken 기능 추가

--- src/Logout.razor.cs
+++ src/Logout.razor.cs
@@ -1,6 +1,10 @@
 ﻿using MetaFrm.Auth;
-using MetaFrm.Control;
+using MetaFrm.Database;
+using MetaFrm.Maui.Devices;
 using MetaFrm.Razor.ViewModels;
+using MetaFrm.Service;
+using MetaFrm.Web.Bootstrap;
+using Microsoft.AspNetCore.Components;
 
 namespace MetaFrm.Razor
 {
@@ -10,6 +14,10 @@ namespace MetaFrm.Razor
     public partial class Logout
     {
         internal LogoutViewModel LogoutViewModel { get; set; } = Factory.CreateViewModel<LogoutViewModel>();
+
+
+        [Inject]
+        internal IDeviceToken? DeviceToken { get; set; }
 
         /// <summary>
         /// OnAfterRenderAsync
@@ -31,6 +39,14 @@ namespace MetaFrm.Razor
 
                     if (this.AuthenticationState != null)
                     {
+                        if (this.DeviceToken != null)
+                        {
+                            string? tmp = await this.DeviceToken.GetToken();
+
+                            if (!tmp.IsNullOrEmpty()) 
+                                this.DeleteToken(tmp);
+                        }
+
                         var auth = this.AuthenticationState.Result;
 
                         if (auth.User.Identity != null && auth.User.Identity.IsAuthenticated)
@@ -57,6 +73,40 @@ namespace MetaFrm.Razor
                 {
                     this.LogoutViewModel.IsBusy = false;
                 }
+            }
+        }
+
+        private async void DeleteToken(string TOKEN_STR)
+        {
+            Response? response;
+
+            try
+            {
+                ServiceData serviceData = new()
+                {
+                    TransactionScope = true,
+                    Token = this.UserClaim("Token")
+                };
+                serviceData["1"].CommandText = this.GetAttribute("DeleteToken");
+                serviceData["1"].AddParameter("TOKEN_TYPE", DbType.NVarChar, 50, "Firebase.FCM");
+                serviceData["1"].AddParameter(nameof(TOKEN_STR), DbType.NVarChar, 200, TOKEN_STR);
+
+                response = await serviceData.ServiceRequestAsync(serviceData);
+
+                if (response.Status != Status.OK)
+                {
+                    if (response.Message != null)
+                    {
+                        this.ModalShow("Warning", response.Message, new() { { "Ok", Btn.Warning } }, null);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                this.ModalShow("An Exception has occurred.", e.Message, new() { { "Ok", Btn.Danger } }, null);
+            }
+            finally
+            {
             }
         }
     }
